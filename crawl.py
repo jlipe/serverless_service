5c8cada4584e994a57313a729ff9a85fcc0b0e57
James Lipe <james@jameslipe.com>
2022-03-09 10:01:56 -0800

    Update crawl

--- crawl.py
+++ crawl.py
@@ -41,12 +41,18 @@ def lambda_handler(event, context):
   kotaku_header = get_kotaku_header()
   put_headline(table, kotaku_header, "Kotaku")
   
+def get_soup(url):
+  try:
+    req = requests.get(url, timeout=6)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    return soup
+  except:
+    return ""
+  
 
 def get_fox_news_header():
   try:
-    url = "https://foxnews.com"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://foxnews.com")
     div = soup.find("div", {"class":"collection collection-spotlight has-hero"})
     header = div.findChildren("h2")
     title = header[0]
@@ -57,9 +63,7 @@ def get_fox_news_header():
     
 def get_cnn_header():
   try:
-    url = "https://www.cnn.com/data/ocs/section/index.html:homepage1-zone-1/views/zones/common/zone-manager.izl"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://www.cnn.com/data/ocs/section/index.html:homepage1-zone-1/views/zones/common/zone-manager.izl")
     headlines = soup.find_all("h2")
     headline = headlines[0]
     return headline.text.strip()
@@ -68,9 +72,7 @@ def get_cnn_header():
     
 def get_cnbc_header():
   try:
-    url = "https://www.cnbc.com/"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://www.cnbc.com/")
     section = soup.find_all("div", {"class":"FeaturedCard-imageContainer"})[0]
     link = section.find("a")
     return link["title"].strip()
@@ -79,9 +81,7 @@ def get_cnbc_header():
   
 def get_breitbart_header():
   try:
-    url = "https://breitbart.com"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://breitbart.com")
     sections = soup.find_all("section", {"class":"top_article_main"})[0]
     headline = sections.find("a")
     return headline["title"]
@@ -90,9 +90,7 @@ def get_breitbart_header():
   
 def get_mother_jones_header():
   try:
-    url = "https://motherjones.com"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://motherjones.com")
     header = soup.find_all("h1", {"class":"hed"})[0]
     return header.text
   except:
@@ -100,9 +98,7 @@ def get_mother_jones_header():
   
 def get_nytimes_header():
   try:
-    url = "http://nytimes.com"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("http://nytimes.com")
     headlines = soup.find_all("h3")
     headline = headlines[0]
     return headline.text
@@ -111,9 +107,7 @@ def get_nytimes_header():
   
 def get_daily_fx_header():
   try:
-    url = "http://dailyfx.com"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("http://dailyfx.com")
     headlines = soup.find_all("p", {"class": "dfx-articleHero__text"})
     headline = headlines[0]
     return headline.text.strip()
@@ -122,9 +116,7 @@ def get_daily_fx_header():
 
 def get_ft_header():
   try:
-    url = "https://www.ft.com/"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://www.ft.com/")
     section = soup.find_all("div", {"class":"headline js-teaser-headline headline--scale-7"})[0]
     title = section.find("span")
     return title.text.strip()
@@ -133,9 +125,7 @@ def get_ft_header():
     
 def get_crypto_news_header():
   try:
-    url = "https://cryptonews.com/"
-    req = requests.get(url, timeout=6) 
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://cryptonews.com/")
     section = soup.find_all("a", {"class":"article__title article__title--main mb-20"})[1]
     return section.text.strip()
   except:
@@ -143,9 +133,7 @@ def get_crypto_news_header():
     
 def get_pc_gamer_header():
   try:
-    url = "https://www.pcgamer.com/news/"
-    req = requests.get(url)
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://www.pcgamer.com/news/")
     section = soup.find_all("figure", {"class":"feature-block-item"})[0]
     span = section.find("span", {"class", "article-name"})
     return span.text.strip()
@@ -154,9 +142,7 @@ def get_pc_gamer_header():
     
 def get_kotaku_header():
   try:
-    url = "https://kotaku.com/culture/news"
-    req = requests.get(url)
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = get_soup("https://kotaku.com/culture/news")
     headlines = soup.find_all("h2")
     return headlines[0].text.strip()
   except:
